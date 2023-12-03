9aef5c27944001bccc677bc0c3a05ed956c23109
Fria Hidayat <frianurulh123@gmail.com>
2023-12-03 16:52:40 +0700

    add constant table name

---
+++ libs/constants/table.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	PermissionTable        = "permissions"
+	RoleTable              = "roles"
+	UserTable              = "users"
+	MonsterCategoryTable   = "monster_categories"
+	MonsterTypeTable       = "monster_types"
+	MonsterTable           = "monsters"
+	MappingMonsterAndTypes = "mapping_monster_and_types"
+)

6d7f3140c428259e18aef895c3ba23ec138ea80e
Fria Hidayat <frianurulh123@gmail.com>
2023-12-03 16:47:58 +0700

    add error message constant

---
+++ libs/constants/error_message.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	BaseURLInvalidEnv   = "invalid base url"
+	AppInvalidEnv       = "invalid app environment"
+	PostgresInvalidEnv  = "invalid postgres database environment"
+	MigrationInvalidEnv = "invalid migration environment"
+	JWTKeyEnv           = "invalid JWT key"
+)
